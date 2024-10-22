68d1c12fb66aa8f7c4b50677f5b01c32a7150f6f
Bhavanakummitha <bhavanareddykummitha@gmail.com>
2024-10-22 09:11:04 +0530

    commiting to Loops

---
+++ Loops/string/Continue statement.py
@@ -0,0 +1,14 @@
+for i in range(6):
+      if i==4:
+            continue
+      print(i)
+      # break statement:
+      for i in range(4):
+          if i==3:
+              break
+          print(i)
+#continue statement:
+for i in range(8):
+    if i==3:
+        continue
+    print(i)
